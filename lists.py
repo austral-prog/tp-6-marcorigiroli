1ac0cd3a7ef08f92a37e5b3dd7808b38f06b820e
marcorigiroli <mrigiroli@mail.austral.edu.ar>
2025-06-06 11:52:08 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,49 @@
-# Replace the "ANSWER HERE" with your answer
+def remove_elements(lista):
+  
+    nueva_lista = lista.copy()
+ 
+    if len(nueva_lista) > 5:
+        del nueva_lista[5]
+   
+    if len(nueva_lista) > 4:
+        del nueva_lista[4]
+    
+    if len(nueva_lista) > 0:
+        del nueva_lista[0]
 
-def remove_elements(list_to_remove_elements):
-    indices_to_remove = [0, 4, 5]
-    return [item for i, item in enumerate(list_to_remove_elements) if i not in indices_to_remove]
+    return nueva_lista
+original = [10, 20, 30, 40, 50, 60, 70]
+resultado = remove_elements(original)
+
+print("Original:", original)
+print("Modificada:", resultado)
 
 
-def add_elements(list_to_add_elements):
-    return ['Pink'] + list_to_add_elements + ['Yellow']
+def add_elements(lista):
+    
+    nueva_lista = lista.copy()
+    nueva_lista.insert(0, 'Pink')
+    nueva_lista.append('Yellow')
+
+    return nueva_lista
+
+def is_empty(lista):
+
+    return len(lista) == 0
 
 
-def is_empty(list_to_check):
-    return len(list_to_check) == 0
+def check_lists(lista1, lista2):
 
-
-def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
-        return list_to_compare1[2] == list_to_compare2[2]
+    if len(lista1) > 2 and len(lista2) > 2:
+        return lista1[2] == lista2[2]
     else:
         return False
 
+def list_of_lists(lista_de_listas):
+    
+    primera = lista_de_listas[0][:2]        
+    segunda = lista_de_listas[1][1:4]       
+    tercera = lista_de_listas[2][-2:]        
 
-def list_of_lists(list_of_lists_to_modify):
-    modified_list = []
-    if len(list_of_lists_to_modify) >= 1:
-        modified_list.append(list_of_lists_to_modify[0][:2])
-    if len(list_of_lists_to_modify) >= 2:
-        modified_list.append(list_of_lists_to_modify[1][1:4])
-    if len(list_of_lists_to_modify) >= 3:
-        modified_list.append(list_of_lists_to_modify[2][-2:])
-    return modified_list
+    
+    return [primera, segunda, tercera]
